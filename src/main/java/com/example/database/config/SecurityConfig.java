74545c11b1e6c05f7c9e0d4272d2c7b00e94252a
GangHyeon <128055955+Lepied@users.noreply.github.com>
2024-11-16 14:20:44 +0900

    feat:권한 알고리즘 변경

--- src/main/java/com/example/database/config/SecurityConfig.java
+++ src/main/java/com/example/database/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.database.config;
 
+import com.example.database.service.MyUserDetailsService;
 import com.example.database.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,19 +20,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig
 {
-
+    private final MyUserDetailsService myUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> // HTTP 요청에 대한 보안 규칙을 정의
                         authorizeRequests
-                                .requestMatchers("/users/login","/users/register","/main").permitAll() // /user/** 경로에 대한 접근을 허용
-                                // 유저 관리 페이지는 "USER" 역할을 가진 사람만 접근 가능
-                                .requestMatchers("/main").authenticated()
-                                .anyRequest().authenticated() // 그 외의 모든 요청은 인증을 요구
+                                .requestMatchers("/**").permitAll() // /** 경로에 대한 접근을 허용
+                               
+                                //.requestMatchers("/main").hasRole("USER")
+                                //anyRequest().authenticated() // 그 외의 모든 요청은 인증을 요구
+
                 )
                 .csrf(AbstractHttpConfigurer::disable); // CSRF 보호를 비활성화
+
 
         return http.build(); // HTTP 보안 설정을 빌드하여 반환
     }
