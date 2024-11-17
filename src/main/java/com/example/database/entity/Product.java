e0a5e3f02a415a27ba33dea5893a4f9cf58474f7
Nangzu <skdwnansrn@naver.com>
2024-11-17 19:59:04 +0900

    실행시 네이버api 불러와짐 호출방식 https://openapi.naver.com/v1/search/shop.json?query=adidas 쿼리다음에 한글안댐 영어무조건

--- src/main/java/com/example/database/entity/Product.java
+++ src/main/java/com/example/database/entity/Product.java
@@ -11,18 +11,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-    private String link;
-    private String image;
-    private Integer lprice;
-    private Integer hprice;
-    private String mallName;
-    private String productId;
-    private Integer productType;
-    private String brand;
-    private String maker;
-    private String category1;
-    private String category2;
-    private String category3;
-    private String category4;
+    private String name;       // 상품명
+    private int price;         // 최저가
+    private String link;       // 상품 상세 링크
+    private String mallName;   // 판매 쇼핑몰 이름
+    private String category1;  // 카테고리 1
+    private String category2;  // 카테고리 2
+    private String category3;  // 카테고리 3
+    private String category4;  // 카테고리 4
 }
--- src/main/java/com/example/database/repository/ProductRepository.java
+++ src/main/java/com/example/database/repository/ProductRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // 상품 이름에 키워드가 포함된 상품 조회
-    List<Product> findByTitleContaining(String keyword);
+    List<Product> findByNameContaining(String keyword);
 
-    // 가격으로 검색
-    List<Product> findByLprice(int lprice);
+    // 특정 가격 범위 내의 상품 조회
+    List<Product> findByPriceBetween(int minPrice, int maxPrice);
 
     // 특정 카테고리의 상품 조회
     List<Product> findByCategory1(String category1);
