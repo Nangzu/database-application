61db2745260b8dd58ecad1afee11252d17359d15
khc312 <147470416+khc312@users.noreply.github.com>
2024-11-24 13:47:10 +0900

    단순 최신화

--- database-application/src/main/java/com/example/database/dto/NaverRequestVariableDto.java
+++ database-application/src/main/java/com/example/database/dto/NaverRequestVariableDto.java
@@ -1,13 +1,8 @@
 package com.example.database.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class NaverRequestVariableDto {
     private String query;
-    private Integer display;
-    private Integer start;
-    private String sort;
 }
