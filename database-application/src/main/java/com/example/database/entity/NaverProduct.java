ffe01f41e6e2c8532b2c49e96722552286964fe3
khc312 <147470416+khc312@users.noreply.github.com>
2024-11-24 13:47:40 +0900

    db 업로드

--- database-application/src/main/java/com/example/database/entity/NaverProduct.java
+++ database-application/src/main/java/com/example/database/entity/NaverProduct.java
@@ -1,92 +1,28 @@
 package com.example.database.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
-@Table(name = "NAVER_PRODUCT")
-public class NaverProduct {
-    
+@Data
+public class Product {
+
     @Id
-    private String productId; // 상품 ID (primary key)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column
     private String title;
-
-    @Column
     private String link;
-
-    @Column
     private String image;
-
-    @Column
-    private int lprice;
-
-    @Column
-    private int hprice;
-
-    @Column
+    private Integer lprice;
+    private Integer hprice;
     private String mallName;
-
-    @Column
+    private String productId;
+    private Integer productType;
     private String brand;
-
-    @Column
+    private String maker;
     private String category1;
-
-    @Column
     private String category2;
-
-    @Column
     private String category3;
-
-    @Column
     private String category4;
-    
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setLprice(int lprice) {
-        this.lprice = lprice;
-    }
-
-    public void setHprice(int hprice) {
-        this.hprice = hprice;
-    }
-
-    public void setMallName(String mallName) {
-        this.mallName = mallName;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public void setCategory1(String category1) {
-        this.category1 = category1;
-    }
-
-    public void setCategory2(String category2) {
-        this.category2 = category2;
-    }
-
-    public void setCategory3(String category3) {
-        this.category3 = category3;
-    }
-
-    public void setCategory4(String category4) {
-        this.category4 = category4;
-    }
 }
